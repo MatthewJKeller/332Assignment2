1da30b11fff28380dba89e30756f6820afc980e9
joshberger5 <bergjo7@furman.edu>
2024-02-21 17:44:33 -0500

    fix for the stereogum.com not static.stereogum.com

--- import dns.py
+++ import dns.py
@@ -8,22 +8,23 @@ def main():
     root_servers = ['198.41.0.4', '170.247.170.2', '192.33.4.12',
     '199.7.91.13', '192.203.230.10','192.5.5.241', '192.112.36.4',
     '198.97.190.53', '192.36.148.17', '192.58.128.30',
-    '193.0.14.129', '199.7.83.42', '202.12.27.33']
-    dnsQuery('stereogum.com', 'A', root_servers)
+    '193.0.14.129', '199.7.83.42', '202.12.27.33', '8.8.8.8']
+    dnsQuery('static.stereogum.com', root_servers)
   
   
-def dnsQuery(name, type, root_servers):
-    query = dns.message.make_query(name, type)
-    for server in root_servers:
-        try:
-            response = dns.query.udp(query, server)
-            print("got it! " + response)
-            break
-        except:
-            print("Trying " + server + " for " + name + " failed") 
-            pass
-        
+def dnsQuery(name, root_servers):
+    """ Gets the IP Address for a given URL using the given nameservers"""
+    query = dns.message.make_query(name, dns.rdatatype.A)   # makes a DNS query for the given URL, type=A
 
+    for server in root_servers:                             # using the given name servers, try to obtain a response
+        print("Asking " + server + " about " + name)
+        response = dns.query.tcp(query, server)             # grab the response for the nameserver
+        for answer in response.answer:
+            for item in answer.items:
+                if item.address != None:
+                    print("Got It! " + item.address)
+                    return                                  # return the first IP address found
+    
 
 if __name__ == "__main__":
     main()
