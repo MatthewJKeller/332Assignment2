ceafdd57ff3fb30e15b53870a92d508dec0d64bb
joshberger5 <bergjo7@furman.edu>
2024-02-26 16:20:44 -0500

    1st part
    You have to get the IP address of the next server to go to in order to find the IP address

--- import dns.py
+++ import dns.py
@@ -8,8 +8,8 @@ def main():
     root_servers = ['198.41.0.4', '170.247.170.2', '192.33.4.12',
     '199.7.91.13', '192.203.230.10','192.5.5.241', '192.112.36.4',
     '198.97.190.53', '192.36.148.17', '192.58.128.30',
-    '193.0.14.129', '199.7.83.42', '202.12.27.33', '8.8.8.8']
-    dnsQuery('static.stereogum.com', root_servers)
+    '193.0.14.129', '199.7.83.42', '202.12.27.33']
+    dnsQuery('stereogum.com', root_servers)
   
   
 def dnsQuery(name, root_servers):
@@ -19,12 +19,14 @@ def dnsQuery(name, root_servers):
     for server in root_servers:                             # using the given name servers, try to obtain a response
         print("Asking " + server + " about " + name)
         response = dns.query.tcp(query, server)             # grab the response for the nameserver
-        for answer in response.answer:
-            for item in answer.items:
-                if item.address != None:
-                    print("Got It! " + item.address)
-                    return                                  # return the first IP address found
-    
+        if response.answer != []:
+            for answer in response.answer:
+                for item in answer.items:
+                    if item.address != None:
+                        print("Got It! " + item.address)
+                        return                                  # return the first IP address found
+        elif response.additional != []:
+            # get the IP address of the next server to go to
 
 if __name__ == "__main__":
     main()
