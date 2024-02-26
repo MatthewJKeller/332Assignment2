c9f19efff3c12380fc57181815d33af434d98f5f
joshberger5 <bergjo7@furman.edu>
2024-02-26 18:33:29 -0500

    else doesn't work

--- import dns.py
+++ import dns.py
@@ -5,31 +5,32 @@ import dns.query
 """Domains tested for debugging: """
 
 def main():
-    root_servers = ['198.41.0.4', '170.247.170.2', '192.33.4.12',
-    '199.7.91.13', '192.203.230.10','192.5.5.241', '192.112.36.4',
-    '198.97.190.53', '192.36.148.17', '192.58.128.30',
-    '193.0.14.129', '199.7.83.42', '202.12.27.33']
-    dnsQuery('stereogum.com', root_servers)
+    root_servers = ['198.41.0.4']
+    dnsQuery('status.stereogum.com', root_servers)
   
   
-def dnsQuery(name, root_servers)->bool:
+def dnsQuery(name: str, root_servers: list)->bool:
     """ Gets the IP Address for a given URL using the given nameservers"""
-    query = dns.message.make_query(name, dns.rdatatype.A)   # makes a DNS query for the given URL, type=A
+    query = dns.message.make_query(name, dns.rdatatype.A)          # makes a DNS query for the given URL, type=A
 
     for i in range(len(root_servers)):                             # using the given name servers, try to obtain a response
         print("Asking " + root_servers[i] + " about " + name)
-        response = dns.query.tcp(query, root_servers[i])             # grab the response for the nameserver
+        response = dns.query.tcp(query, root_servers[i])           # grab the response for the nameserver
         if response.answer != []:
             for answer in response.answer:
                 for item in answer.items:
                     if item.address != None:
                         print("Got It! " + item.address)
-                        return True                                 # return the first IP address found
-        elif response.additional != []: # get the IP address of the next server to go to
+                        return True                                # return the first IP address found
+        elif response.additional != []:                            # get the IP address of the next server to go to
             lst = []
             lst.append(str(response.additional[0][0]))
-            if dnsQuery(name, lst):
+            if dnsQuery(name, lst):                                # perform the query for the next server
                 return True
+        else:
+            query = dns.message.make_query(name, dns.rdatatype.CNAME) 
+            response = dns.query.tcp(query, root_servers[i])
+            print(response.authority[0][0])
 
 if __name__ == "__main__":
     main()
