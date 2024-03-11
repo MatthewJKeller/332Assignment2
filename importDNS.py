62d8ceaf2d4835b07ba092e90ea22e9a169b62c1
joshberger5 <bergjo7@furman.edu>
2024-03-11 16:31:42 -0400

    final

--- importDNS.py
+++ importDNS.py
@@ -2,11 +2,11 @@
 import dns.message
 import dns.query
 
-"""Domains tested for debugging:gamefaqs.gamespot.com, stereogum.com, static.stereogum.com, google.com, facebook.com, youtube.com, wikipedia.org, amazon.com, reddit.com, instagram.com, yahoo.com, microsoft.com, bbc.com, wired.com, finance.yahoo.com, usa.gov, gov.uk, europa.eu, twitch.tv"""
+"""Domains tested for debugging: gamefaqs.gamespot.com, stereogum.com, static.stereogum.com, google.com, facebook.com, youtube.com, wikipedia.org, amazon.com, reddit.com, instagram.com, yahoo.com, microsoft.com, bbc.com, wired.com, finance.yahoo.com, usa.gov, gov.uk, europa.eu, twitch.tv"""
 
 def main():
     root_servers = ['198.41.0.4', '170.247.170.2', '192.33.4.12', '199.7.91.13', '192.203.230.10','192.5.5.241', '192.112.36.4', '198.97.190.53', '192.36.148.17', '192.58.128.30', '193.0.14.129', '199.7.83.42', '202.12.27.33']
-    dnsQuery('static.stereogum.com', root_servers, 0, 'static.stereogum.com')
+    dnsQuery('google.com', root_servers, 0, 'google.com')
   
 def dnsQuery(name: str, root_servers: list, jumps: int, initialName: str):
     """ Gets the IP Address for a given URL using the given nameservers"""
@@ -23,19 +23,15 @@ def dnsQuery(name: str, root_servers: list, jumps: int, initialName: str):
                 if name != initialName:
                     lst = []
                     lst.append(str(response.answer[0][0]))
-                    result= dnsQuery(initialName, lst, jumps+1, initialName)
-                    if result == True:                                     # if you got the actual IP address from one of the recursive calls, return true                
-                        return result
-                    elif result != None:                                   # if you got the CNAME from one of the recursive calls, set name to the CNAME
-                        name = result
-                        if jumps != 0:                                     # return name if you are still in a recursive call
-                            return name
+                    result = dnsQuery(initialName, lst, jumps+1, initialName)
+                    return result
                 else:
                     print('Got It! ' + str(response.answer[0][0]))
                     return True  
             elif str(response.answer[0]).find(' IN CNAME ') != -1: # get the CNAME if there was not an IP address
                 name = str(response.answer[0][0])[:-1]
-                return name
+                initialName = name
+                return [name, initialName]
         elif response.additional != []:                            # get the IP address of the next server to go to and do the query on it recursively
             lst = []
             if str(response.additional[0][0]).find(':') != -1:     # do not use the IPv6 if given at [0][0], grab the IPv4
@@ -43,16 +39,24 @@ def dnsQuery(name: str, root_servers: list, jumps: int, initialName: str):
             else:
                 lst.append(str(response.additional[0][0]))
             result = dnsQuery(name, lst, jumps+1, initialName)
-            if result == True:                                     # if you got the actual IP address from one of the recursive calls, return true                
+            if result == True:                                             # if you got the actual IP address from one of the recursive calls, return true                
                 return result
             elif result != None:                                   # if you got the CNAME from one of the recursive calls, set name to the CNAME
-                name = result
+                name = result[0]
+                initialName = result[1]
                 if jumps != 0:                                     # return name if you are still in a recursive call
-                    return name
-        elif response.authority != []:                            # get the CNAME, if in recursive call, return it, if not, set name to CNAME
-            name = str(response.authority[0][0])[:-1]
-            if jumps != 0:
-                return name
+                    return [name, initialName]
+        elif response.authority != []:                             
+            if str(response.authority[0]).find(' IN CNAME ') != -1: # get the CNAME, if in recursive call, return it, if not, set name to CNAME
+                name = str(response.authority[0][0])[:-1]
+                initialName = name
+                if jumps != 0:
+                    return [name, initialName]
+            elif str(response.authority[0]).find(' IN NS ') != -1: # get the NS, if in recursive call, return it, if not, set name to NS
+                name = str(response.authority[0][0])[:-1]
+                if jumps != 0:
+                    return [name, initialName]
+
             
 
 if __name__ == "__main__":
